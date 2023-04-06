83e0da73e2ef412b2f82f303fef9a6042fed90bc
duyvo3001 <74234968+duyvo3001@users.noreply.github.com>
2023-04-07 00:53:08 +0700

    Update DropdownSetting.js
    modifi delete handle

--- client/src/pages/Import/table/DropdownSetting.js
+++ client/src/pages/Import/table/DropdownSetting.js
@@ -1,13 +1,23 @@
+import { useEffect } from 'react';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Request from '../../../api/Request'
 
-const handleClick = () =>{
-// xu ly khi clicked
+const handleClick = () => {
+    // xu ly khi clicked
 }
+const HandleClickDelete = (id) => {
+    // xu ly khi clicked
+    useEffect(()=>{
+        Request.post(`/deleteBrand/${id}`, { headers: { Authorization: sessionStorage.getItem("access_token") } })
+        .then()
+        .catch(eror =>{console.error(eror)})
+    })
 
+}
 const DropdownSetting = (props) => {
     return <NavDropdown title="Action" id="basic-nav-dropdown" >
         <NavDropdown.Item onClick={handleClick} id={props.ID} >Edit</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2" id={props.ID}>Delete</NavDropdown.Item>
+        <NavDropdown.Item onClick={HandleClickDelete(props.ID)} id={props.ID}>Delete</NavDropdown.Item>
     </NavDropdown>
 }
 export default DropdownSetting
