13738bd09252f819e95b7ed4daa6c6430ebb66c4
duyvo <duyvo3001@gmail.com>
2023-08-24 16:17:52 +0700

    update dashboard

--- client/src/pages/dashBoard/DashBoard.js
+++ client/src/pages/dashBoard/DashBoard.js
@@ -5,9 +5,64 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Request from "../../../src/api/Request";
-
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
 const DashBoard = () => {
-
+    const [rows,setrows] = useState([{ id:0 , MaLK: 'null', TenLK: 'null', Soluong: 0,NgayNhap:"null", TinhTrangHang: "null" }])
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 90 },
+        {
+            field: 'MaLK',
+            headerName: 'ID Product',
+            width: 300,
+            editable: false,
+        },
+        {
+            field: 'TenLK',
+            headerName: 'Name Product',
+            width: 600,
+            editable: false,
+        },
+        {
+            field: 'Soluong',
+            headerName: 'Quantity',
+            type: 'number',
+            width: 150,
+            editable: false,
+        },
+        {
+            field: 'NgayNhap',
+            headerName: 'Day Import',
+            width: 200,
+            editable: false,
+        },
+        {
+            field: 'TinhTrangHang',
+            headerName: 'Stock status',
+            description: 'This column has a value getter and is not sortable.',
+            sortable: false,
+            width: 200,
+        },
+    ];
+        
+    function DataGridDemo() {
+        return (
+            <Box sx={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: {
+                                pageSize: 10,
+                            },
+                        },
+                    }}
+                    pageSizeOptions={[5]}
+                />
+            </Box>
+        );
+    }
     const [count, setCount] = useState({
         Inventory: 0,
         InventoryQuantity: 0,
@@ -20,6 +75,7 @@ const DashBoard = () => {
             { headers: { Authorization: sessionStorage.getItem("access_token") } })
             .then((response) => {
                 setCount(response?.data?.result)
+                setrows(response?.data?.ProductObj)
             })
             .catch((error) => { console.error(error) })
     }, [])
@@ -96,8 +152,9 @@ const DashBoard = () => {
                 </Grid>
             </Grid>
             <Typography color="black" className="mb-3">
-                Recent Items
+                Recent product
             </Typography>
+            <DataGridDemo />
         </Container>
     )
 }
