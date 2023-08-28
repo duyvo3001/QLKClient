b1c2ed0f1ef10cf121c24ad7216ab675116b775c
duyvo3001 <74234968+duyvo3001@users.noreply.github.com>
2023-08-29 00:12:59 +0700

    check

---
+++ client/src/pages/report/Outofstock/ReportOutOfStockPage.js
@@ -0,0 +1,274 @@
+import { useEffect, React, useState, useRef } from 'react'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import ReactToPrint from 'react-to-print';
+import Request from "../../../../src/api/Request"
+import OutOfStockTable from "../Outofstock/table/OutOfStockTable"
+import { Button } from '@mui/material';
+import Container from '@mui/material/Container';
+import "../../report/scss/style.scss"
+
+const ReportOutOfStockPage = () => {
+  return (
+    <Outofstock />
+  )
+}
+const Outofstock = () => {
+  const componentRef = useRef();
+  const Stockstatus = [
+    { key: "Stockstatus", label: 'GOOD' },
+    { key: "Stockstatus", label: 'BAD' },
+    { key: "Stockstatus", label: 'CANCEL' }
+  ];
+  const [DataBrand, setDataBrand] = useState([])
+  const [DataCategory, setDataCategory] = useState([])
+  const [DataWarehouse, setDataWarehouse] = useState([]);
+  const [DataStock, setDataStock] = useState([]);
+  const [DataProduct, setDataProduct] = useState([]) //state dataProduct
+
+  const [formData, setFormData] = useState({
+    MaThuongHieu: "",
+    MaLK: "",
+    Category: "",
+    MaKho: "",
+    TinhTrangHang: "",
+  });
+  const [filters, setfilters] = useState({
+    page: 1,
+  });
+  const [pageindex, setpageindex] = useState({
+    page: 1,
+  });
+
+  const HandleButtonClick = (newPage) => {
+    setfilters({
+      ...filters,
+      page: newPage,
+    });
+    setpageindex({ ...pageindex, page: newPage });
+  };
+
+  function RequestRouterSearch(Url, keyName, SetData) {
+    Request
+      .get(`/${Url}`,
+        { headers: { Authorization: sessionStorage.getItem("access_token") } })
+      .then((response) => {
+        const object = []
+        response?.data?.result?.map((key) => {
+          return object.push({ label: key?.[keyName], key: keyName })
+        })
+        SetData(object)
+      })
+      .catch((error) => { console.log(error) })
+  }
+  useEffect(() => {
+    RequestRouterSearch("SearchStock", "MaLK", setDataStock)
+    RequestRouterSearch("SearchCategory", "Category", setDataCategory)
+    RequestRouterSearch("SearchBrand", "MaThuongHieu", setDataBrand)
+    RequestRouterSearch("SearchWarehouse", "MaKho", setDataWarehouse)
+  }, [])
+
+  useEffect(() => {
+    Request
+      .post(`/OutofStock`,
+        { formData },
+        { headers: { Authorization: sessionStorage.getItem("access_token") } })
+      .then((response) => { setDataProduct(response.data.result) })
+      .catch((error) => { console.log(error) })
+  }, [formData])
+
+  const HandleChange = (event, newvalue) => {
+    console.log(newvalue)
+    if (newvalue) {
+      switchUpdate(newvalue)
+    }
+  }
+
+  function switchUpdate(newvalue) {
+    switch (newvalue?.key || newvalue) {
+      case "MaThuongHieu":
+        updatePrestaste("MaThuongHieu", newvalue)
+        break;
+      case "Category":
+        updatePrestaste("Category", newvalue)
+        break;
+      case "MaKho":
+        updatePrestaste("MaKho", newvalue)
+        break;
+      case "MaLK":
+        updatePrestaste("MaLK", newvalue)
+        break;
+      default:
+        updatePrestaste("TinhTrangHang", newvalue)
+        break;
+    }
+  }
+
+  function updatePrestaste(params, newvalue) {
+    setFormData(prevState => ({
+      ...prevState,
+      [params]: newvalue?.label || ""
+    }));
+  }
+  const StockOnclose = (event, newvalue) => {
+    if (newvalue === "") {
+      switchUpdate("MaLK")
+    }
+  }
+  const MaKhoOnclose = (event, newvalue) => {
+    if (newvalue === "") {
+      switchUpdate("MaKho")
+    }
+  }
+  const MaThuongHieuOnclose = (event, newvalue) => {
+    if (newvalue === "") {
+      switchUpdate("MaThuongHieu")
+    }
+  }
+  const CategoryOnclose = (event, newvalue) => {
+    if (newvalue === "") {
+      switchUpdate("Category")
+    }
+  }
+  const TinhTrangHangOnclose = (event, newvalue) => {
+    if (newvalue === "") {
+      switchUpdate("TinhTrangHang")
+    }
+  }
+
+  return (
+    <>
+      <Container>
+
+        <Row className='mb-2 row'>
+          <Col className="mb-3">
+            <h3>Out of stock</h3>
+          </Col>
+        </Row>
+        <Row className='mb-2 row'>
+          <Col md={6} sm={12} lg={6}>
+            <Autocomplete
+              fullWidth={true}
+              loading={true}
+              disablePortal
+              id="Product"
+              size="small"
+              options={DataStock}
+              sx={{ width: 580 }}
+              onChange={HandleChange}
+              onInputChange={StockOnclose}
+              name="Product"
+              renderInput={(params) => <TextField {...params} label="Product" name="Product" />}
+            /></Col>
+          <Col md={6} sm={12} lg={6}>
+            <Autocomplete
+              fullWidth={true}
+              loading={true}
+              disablePortal
+              id="MaKho"
+              size="small"
+              options={DataWarehouse}
+              sx={{ width: 190 }}
+              onChange={HandleChange}
+              onInputChange={MaKhoOnclose}
+              name="MaKho"
+              renderInput={(params) => <TextField {...params} label="Ware House" name="Hang" />}
+            /></Col>
+        </Row>
+        <Row className='mb-2 row'>
+          <Col md={5} sm={5} lg={2}>
+            <Autocomplete
+              disablePortal
+              id="test"
+              fullWidth={true}
+              size="small"
+              options={DataCategory}
+              sx={{ width: 190 }}
+              onChange={HandleChange}
+              onInputChange={CategoryOnclose}
+              name={"ID"}
+              renderInput={
+                (params) => <TextField {...params}
+                  label="Category"
+                  name={"Category"}
+                />
+              }
+            /></Col>
+          <Col md={5} sm={5} lg={2}>
+            <Autocomplete
+              disablePortal
+              id="test"
+              fullWidth={true}
+              size="small"
+              options={DataBrand}
+              sx={{ width: 190 }}
+              onChange={HandleChange}
+              onInputChange={MaThuongHieuOnclose}
+              name={"ID"}
+              renderInput={
+                (params) => <TextField {...params}
+                  label="ID Brand"
+                  name={"Brand"}
+                />
+              }
+            /></Col>
+          <Col md={5} sm={5} lg={2}>
+            <Autocomplete
+              disablePortal
+              id="test"
+              fullWidth={true}
+              size="small"
+              options={Stockstatus}
+              sx={{ width: 190 }}
+              onChange={HandleChange}
+              onInputChange={TinhTrangHangOnclose}
+              name={"ID"}
+              renderInput={
+                (params) => <TextField {...params}
+                  label="Stock status"
+                  name={"Brand"}
+                />
+              }
+            /></Col>
+          <Col md={5} sm={5} lg={2}>
+            <ReactToPrint
+              trigger={() => {
+                return (<Button className='mb-3' sx={{ width: 190 }} variant='contained'>Print Invoice</Button>)
+              }}
+              content={() => componentRef.current}
+              documentTitle='Invoice'
+              pageStyle='print'
+            />
+          </Col>
+        </Row>
+      </Container>
+
+      {/* <ButtonBottom pageindex={pageindex}
+                    HandleButtonClick={HandleButtonClick} /> */}
+      <div className='tablestyle' ref={componentRef}>
+        <div><h3>Out of stock</h3></div>
+        <div>
+          <Row>
+            <Col md={2} sm={1} lg={2}>Category</Col>
+            <Col md={2} sm={1} lg={2}>: {formData.Category}</Col>
+          </Row>                <Row>
+            <Col md={2} sm={1} lg={2}>Brand</Col>
+            <Col md={2} sm={1} lg={2}>: {formData.MaThuongHieu}</Col>
+          </Row>
+          <Row>
+            <Col md={2} sm={1} lg={2}>Ware house</Col>
+            <Col md={2} sm={1} lg={2}>: {formData.MaKho}</Col>
+          </Row>
+          <Row>
+            <Col md={2} sm={1} lg={2}>Stock status</Col>
+            <Col md={2} sm={1} lg={2}>: {formData.TinhTrangHang}</Col>
+          </Row>
+        </div>
+        <OutOfStockTable DataProduct={DataProduct} />
+      </div>
+    </>
+  )
+}
+export default ReportOutOfStockPage
--- client/src/pages/userManegement/UserSetting.js
+++ client/src/pages/userManegement/UserSetting.js
@@ -1,4 +1,5 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
+import Request from '../../api/Request';
 import { Container } from '@mui/material'
 import { Grid } from '@mui/material';
 import Button from '@mui/material/Button';
@@ -8,9 +9,84 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
+import { AlterShowEror, AlterShowSuccess } from '../../components/Alter/AlterShow';
 const UserSetting = () => {
   const [formData, setformData] = useState({})
+  const [pwData, setpwData] = useState({
+    pass_nv: "",
+    repass_nv: "",
+    label: "Re password",
+    eror: false
+  })
+  const [Show, setShow] = useState({
+    valueShow: false,
+    message: ""
+  });
+  const [ShowEror, setShowEror] = useState({
+    valueShow: false,
+    message: ""
+  });
   const [valueform, setvalueform] = useState({})
+
+  const handlechange = (event) => {
+    const { name, value } = event.target
+    setformData({ ...formData, [name]: value })
+  }
+
+  useEffect(() => { // set eror re password 
+    if (pwData.pass_nv !== pwData.repass_nv) {
+      setpwData({ ...pwData, eror: true, label: "Incorrect password" })
+    }
+    else {
+      setpwData({ ...pwData, eror: false, label: "Re password" })
+    }
+  }, [pwData.pass_nv, pwData.repass_nv])
+
+  const handlechangePass = (event) => {
+    const { name, value } = event.target
+    console.log(name, value)
+    if (name === "repass_nv")
+      setpwData({ ...pwData, repass_nv: value })
+    else
+      setpwData({ ...pwData, pass_nv: value })
+  }
+
+  const handleDataPass = () => {
+    if (pwData.pass_nv !== pwData.repass_nv) {
+      setShowEror({
+        valueShow: true,
+        message: "Password and re-password are not the same"
+      })
+    }
+    else {
+      Request
+        .patch("/updateUser",
+          {
+            formData: {
+              pass_nv: pwData.pass_nv,
+              repass_nv: pwData.repass_nv
+            }
+          },
+          { headers: { Authorization: sessionStorage.getItem("access_token") } })
+        .then(response => {
+          if (response?.status === 200) {
+            setShow({
+              valueShow: true,
+              message: "update password successfully"
+            })
+          }
+        })
+        .catch((error) => {
+          setShowEror({
+            valueShow: true,
+            message: error
+          })
+        });
+    }
+  }
+  const handleDataInfo = () => {
+
+  }
   return (
     <>
       <Container>
@@ -20,13 +96,14 @@ const UserSetting = () => {
         <Typography >Personal Information</Typography>
         <Grid className="mb-3" container spacing={{ xs: 2, md: 3 }} columns={{ xs: 5, sm: 8, md: 12 }}>
           <Grid item xs={6} sm={6} md={6}>
-            <TextField size="small" sx={{ minWidth: 500 }} name='TenNV' label="Name"></TextField></Grid>
+            <TextField error={false} size="small" sx={{ minWidth: 500 }} name='TenNV' onChange={handlechange} label="Name"></TextField></Grid>
           <Grid item xs={6} sm={6} md={6}>
             <FormControl>
               <RadioGroup
                 row
                 aria-labelledby="demo-row-radio-buttons-group-label"
                 name='GioiTinh'
+                onChange={handlechange}
               >
                 <FormControlLabel value="female" control={<Radio />} label="Female" />
                 <FormControlLabel value="male" control={<Radio />} label="Male" />
@@ -36,29 +113,37 @@ const UserSetting = () => {
         </Grid>
         <Grid className="mb-3" container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           <Grid item xs={6} sm={6} md={6}>
-            <TextField size="small" sx={{ minWidth: 500 }} label="Email"></TextField></Grid>
+            <TextField size="small" name='Email' sx={{ minWidth: 500 }} onChange={handlechange} label="Email"></TextField></Grid>
           <Grid item xs={6} sm={6} md={6}>
-            <TextField size="small" sx={{ minWidth: 500 }} label="Phone"></TextField></Grid>
+            <TextField size="small" name='SDT' sx={{ minWidth: 500 }} onChange={handlechange} type={'number'} label="Phone"></TextField></Grid>
         </Grid>
         <Grid className="mb-3" container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           <Grid item xs={6} sm={6} md={6}>
             <TextField size="small" multiline
-              rows={4} sx={{ minWidth: 500 }} label="Address"></TextField></Grid>
+              rows={4} sx={{ minWidth: 500 }} name='DiaChi' onChange={handlechange} label="Address"></TextField></Grid>
         </Grid>
         <Grid className="mb-3" container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           <Grid item xs={6} sm={6} md={6}>
-            <Button color="error" size="small" variant="contained">SAVE CHANGE</Button></Grid>
+            <Button color="error" size="small" onClick={handleDataInfo} variant="contained">SAVE CHANGE</Button></Grid>
         </Grid>
+        <AlterShowSuccess Show={Show} setShow={setShow} />
+        <AlterShowEror ShowEror={ShowEror} setShowEror={setShowEror} />
         <Typography> Change Password</Typography>
         <Grid className="mb-3" container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           <Grid item xs={6} sm={6} md={6}>
-            <TextField size="small" type="password" sx={{ minWidth: 500 }} label="Password"></TextField></Grid>
+            <TextField size="small" name='pass_nv' type="password"
+              sx={{ minWidth: 500 }} onChange={handlechangePass}
+              label="Password"></TextField></Grid>
           <Grid item xs={6} sm={6} md={6}>
-            <TextField size="small" type="password" sx={{ minWidth: 500 }} label="Re Password"></TextField></Grid>
+            <TextField size="small" name='repass_nv'
+              type="password" sx={{ minWidth: 500 }}
+              onChange={handlechangePass}
+              error={pwData.eror}
+              label={pwData.label}></TextField></Grid>
         </Grid>
         <Grid className="mb-3" container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           <Grid item xs={6} sm={6} md={6}>
-            <Button color="error" size="small" variant="contained">SAVE CHANGE</Button></Grid>
+            <Button color="error" size="small" onClick={handleDataPass} variant="contained">SAVE CHANGE</Button></Grid>
         </Grid>
       </Container>
     </>
